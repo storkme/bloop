cf117d35038266c2313f4c2ae2c4ddd6348bfe98
alex <haha go away>
2017-04-22 14:22:13 +0100

    welp

---
+++ index.js
@@ -0,0 +1,43 @@
+const bleno = require('bleno');
+
+bleno.on('stateChange', function (state) {
+  console.log('on -> stateChange: ' + state);
+
+  if (state === 'poweredOn') {
+    bleno.startAdvertising('echo', ['ec00']);
+  } else {
+    bleno.stopAdvertising();
+  }
+});
+
+bleno.on('advertisingStart', function (error) {
+  console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
+
+  if (!error) {
+    bleno.setServices([
+      new BlenoPrimaryService({
+        uuid: 'ec00',
+        characteristics: [
+          new (bleno.Characteristic)({
+            uuid: '2A6E',
+            properties: ['read'],
+            descriptors: [
+              new Descriptor({
+                uuid: '2901',
+                value: 'temp level'
+              }),
+              new Descriptor({
+                uuid: '2904',
+                value: new Buffer([0x04, 0x01, 0x27, 0xAD, 0x01, 0x00, 0x00]) // maybe 12 0xC unsigned 8 bit
+              })
+            ]
+          })
+        ],
+        onReadRequest: (offset, callback) => {
+          console.log('read request!!!');
+          callback(0x00, new Buffer([0x10]));
+        }
+      })
+    ]);
+  }
+});
